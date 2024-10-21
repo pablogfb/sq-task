57b565c5448ac181ee187bf7ddda70b62a64ba22
pablo <pablobuntu@gmail.com>
2024-10-21 12:52:25 +0200

    Fix merchant uniqueness vailidation

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'validations' do
+    # Needed for uniqueness validation
+    subject { Merchant.new(reference: "Corp inc",
+                          email: "info@corp-inc.com",
+                          live_on: Date.new(2022, 01, 01),
+                          disbursement_frequency: 2,
+                          minimum_monthly_fee: 29.0)
+            }
     it { should validate_presence_of(:reference) }
     it { should validate_uniqueness_of(:reference) }
     it { should validate_presence_of(:email) }
